87b3de00027e6dd89f7dc5fd353ad7ec707bcac1
Felix Palmer <felixpalmer@gmail.com>
2014-11-03 13:15:38 +0100

    Set up rendering loop

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,10 @@
 define( [ 'quad' ], function ( quad ) {
   var app = {
     init: function () {
+      app.render();
+    },
+    render: function() {
+      requestAnimationFrame( app.render );
       quad.draw();
     }
   };
