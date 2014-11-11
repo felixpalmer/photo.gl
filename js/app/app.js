f726bd595fff9da419854499fe135db203aa8760
Felix Palmer <felixpalmer@gmail.com>
2014-11-11 21:18:27 +0100

    Only draw every tenth frame

--- js/app/app.js
+++ js/app/app.js
@@ -3,21 +3,30 @@ define( [ 'dat', 'params', 'quad' ], function ( dat, params, quad ) {
     init: function () {
       // Load in image to display
       var loadImage = function() {
-        quad.loadImage( 'js/textures/' + params.image, app.render );
-      }
+        quad.loadImage( 'js/textures/' + params.image, onGuiChange );
+      };
+
+      var onGuiChange = function() {
+        app.needsRender = true;
+      };
 
       // Configure controls
       var gui = new dat.GUI();
-      gui.add( params, 'blackAndWhite' ).onChange( app.render );
+      gui.add( params, 'blackAndWhite' ).onChange( onGuiChange );
       gui.add( params, 'image', ['cat.jpg', 'goat.jpg', 'mountain.jpg'] ).onChange( loadImage );
       loadImage();
 
       // Kick off rendering loop
       app.render();
     },
+    frame: 0,
+    needsRender: true,
     render: function() {
       requestAnimationFrame( app.render ); // Enable for continuous rendering
-      quad.draw();
+      if ( app.needsRender || ( app.frame++ % 10 ) === 1 ) { // Unless forced, only render every tenth frame
+        app.needsRender = false;
+        quad.draw();
+      }
     }
   };
 
